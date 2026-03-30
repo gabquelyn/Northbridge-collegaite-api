640d38e7de95c977e69160f943227dd1458ef18a
Arebamen <gabquelyn@gmail.com>
2026-03-30 09:32:15 +0100

    removed validation on email content

--- src/routes/consultation.ts
+++ src/routes/consultation.ts
@@ -3,17 +3,6 @@ import { inquire } from "../controllers/consultation/inquire";
 import { body } from "express-validator";
 const consultationRouter = Router();
 
-consultationRouter.post(
-  "/",
-  [
-    body("fullName").notEmpty().escape(),
-    body("email").isEmail(),
-    body("phoneNumber").isMobilePhone("any"),
-    body("country").notEmpty().escape(),
-    body("academicBackground").notEmpty().escape(),
-    body("pathway").notEmpty().escape(),
-  ],
-  inquire,
-);
+consultationRouter.post("/", inquire);
 
 export default consultationRouter;
