f6e0cf7642f46ccb66b0bd4461b86d676751f302
Arebamen <gabquelyn@gmail.com>
2026-03-30 09:48:54 +0100

    to trigger a workflow for env

--- src/routes/consultation.ts
+++ src/routes/consultation.ts
@@ -1,6 +1,5 @@
 import Router from "express";
 import { inquire } from "../controllers/consultation/inquire";
-import { body } from "express-validator";
 const consultationRouter = Router();
 
 consultationRouter.post("/", inquire);
