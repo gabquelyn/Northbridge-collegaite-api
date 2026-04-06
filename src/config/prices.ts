4a2641858bd7669aacd09ba6142aaa46b296e4c9
Arebamen <gabquelyn@gmail.com>
2026-04-06 20:44:53 +0100

    updated the prices

--- src/config/prices.ts
+++ src/config/prices.ts
@@ -4,11 +4,11 @@
       amount: number;
       detail: string;
     }[] = [
-      { name: "CAAP", amount: 3700000, detail: "Canadian Alignment Program" },
-      { name: "GRADE11", amount: 11892550, detail: "Grade 11" },
-      { name: "GRADE12", amount: 11892550, detail: "Grade 12" },
+      { name: "CAAP", amount: 2960000, detail: "Canadian Alignment Program" },
+      { name: "GRADE11", amount: 10110000, detail: "Grade 11" },
+      { name: "GRADE12", amount: 10110000, detail: "Grade 12" },
       { name: "AY12", amount: 3700000, detail: "AY 12" },
     ];
 
-    export const UNIT_COURSE = 1045000
-    export const APPLICATION_FEE = 1395000
+    export const UNIT_COURSE = 888250
+    export const APPLICATION_FEE = 967000
