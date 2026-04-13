551dd8b91a0ad1101caa252ec336011b00c28d77
Arebamen <gabquelyn@gmail.com>
2026-04-13 22:27:47 +0100

    filtered caap from courses

--- src/utils/getMoodleCached.ts
+++ src/utils/getMoodleCached.ts
@@ -1,15 +1,19 @@
 import { cache } from "../middlewares/cache";
-import { getMoodleCourses } from "./moodle";
-export async function getCachedMoodleCourses(): Promise<{ id: number, fullname: string }[]> {
+import { getCoursesByCategory, getMoodleCourses } from "./moodle";
+export async function getCachedMoodleCourses(): Promise<
+  { id: number; fullname: string }[]
+> {
   const cacheKey = "moodle_courses";
 
-  const cached = cache.get<{ id: number, fullname: string }[]>(cacheKey);
+  const cached = cache.get<{ id: number; fullname: string }[]>(cacheKey);
   if (cached) {
     return cached;
   }
 
-  const courses = await getMoodleCourses();
-
+  const allCourses = await getMoodleCourses();
+  const caapCourses = (await getCoursesByCategory(2)).map((c) => c.id);
+  const caapSet = new Set(caapCourses);
+  const courses = allCourses.filter((c) => !caapSet.has(c.id));
   cache.set(cacheKey, courses);
 
   return courses;
