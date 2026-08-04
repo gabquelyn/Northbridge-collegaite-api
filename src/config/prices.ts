57d16582fe8e05acf933f80aac75bdbcb100095a
Arebamen <gabquelyn@gmail.com>
2026-08-04 04:08:39 +0100

    updated prices

--- src/config/prices.ts
+++ src/config/prices.ts
@@ -1,15 +1,15 @@
-    // program prices
-    export const prices: {
-      name: APPLICATION_PROGRAMS;
-      amount: number;
-      detail: string;
-    }[] = [
-      { name: "CAAP", amount: 6000000, detail: "Canadian Alignment Program" },
-      { name: "GRADE11", amount: 9605000, detail: "Grade 11" },
-      { name: "GRADE12", amount: 9605000, detail: "Grade 12" },
-      { name: "DIRECT", amount: 650000, detail: "Direct Entry Pathway" },
-      { name: "AY12", amount: 3700000, detail: "AY 12" },
-    ];
+// program prices
+export const prices: {
+  name: APPLICATION_PROGRAMS;
+  amount: number;
+  detail: string;
+}[] = [
+  { name: "CAAP", amount: 2_060_000, detail: "Canadian Alignment Program" },
+  { name: "GRADE11", amount: 9_605_000, detail: "Grade 11" },
+  { name: "GRADE12", amount: 9_605_000, detail: "Grade 12" },
+  { name: "DIRECT", amount: 6_750_000, detail: "Direct Entry Pathway" },
+  { name: "AY12", amount: 3_700_000, detail: "AY 12" },
+];
 
-    export const UNIT_COURSE = 665000
-    export const APPLICATION_FEE = 195000
+export const UNIT_COURSE = 665000;
+export const APPLICATION_FEE = 195000;
