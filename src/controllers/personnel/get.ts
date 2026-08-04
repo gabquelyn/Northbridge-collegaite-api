640c76f92f6fa118c286b12c3cb1f75cf8a357dc
Arebamen <gabquelyn@gmail.com>
2026-08-04 16:23:24 +0100

    sorted the personnels

--- src/controllers/personnel/get.ts
+++ src/controllers/personnel/get.ts
@@ -3,7 +3,11 @@ import { Request, Response } from "express";
 import personnel from "../../model/personnel";
 const getPersonnelHandler = expressAsyncHandler(
   async (req: Request, res: Response): Promise<any> => {
-    const personnels = await personnel.find({}).lean().exec();
+    const personnels = await personnel
+      .find({})
+      .sort({ createdAt: -1 })
+      .lean()
+      .exec();
     return res.status(200).json({ personnels });
   },
 );
